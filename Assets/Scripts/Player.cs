76cc375c8b5754b307cb6314809f3b9109e3f49c
Kai Kuehner <kvk@pobox.com>
2019-03-16 16:37:48 -0400

    rolling and jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,79 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+	private const float ROLL_SPEED = 10f;
+	private const float JUMP_SPEED = 10f;
+
+	private const float JUMP_GRACE_TIME = 0.1f;
+
 	private Rigidbody2D rb;
+	private Vector2 lastGroundAngle;
+	private HashSet<GameObject> collisions;
+	private bool canJump = false;
 
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		collisions = new HashSet<GameObject>();
 	}
-	
+
+	private void Update()
+	{
+		if (Input.GetButton("Jump")) //TODO: time window before touching ground
+		{
+			if (canJump)
+			{
+				Jump();
+			}
+		}
+	}
+
+	private void Jump()
+	{
+		rb.AddForce(lastGroundAngle.normalized * JUMP_SPEED, ForceMode2D.Impulse);
+		canJump = false;
+	}
+
 	private void FixedUpdate()
 	{
 		float rotation = Input.GetAxis("Horizontal");
-		rb.AddTorque(rotation);
+		if (rotation != 0)
+		{
+			rb.AddTorque(-rotation * ROLL_SPEED);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		OnCollide(collision);
+	}
+
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		OnCollide(collision);
+	}
+
+	private void OnCollide(Collision2D collision)
+	{
+		lastGroundAngle = collision.GetContact(0).normal;
+		collisions.Add(collision.gameObject);
+		canJump = true;
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		collisions.Remove(collision.gameObject);
+		if (collisions.Count == 0)
+		{
+			Invoke("ClearJump", JUMP_GRACE_TIME);
+		}
+	}
+
+	private void ClearJump()
+	{
+		if (collisions.Count == 0)
+		{
+			canJump = false;
+		}
 	}
 }
