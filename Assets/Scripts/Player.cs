bec3a0027e980eea31d010d340f34c3fb65d974b
Kai Kuehner <kvk@pobox.com>
2019-03-16 15:40:37 -0400

    unity setup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	private Rigidbody2D rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	private void FixedUpdate()
+	{
+		float rotation = Input.GetAxis("Horizontal");
+		rb.AddTorque(rotation);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RollyOlly
+
+Game for Jesse Schell's Game Design class.
+
+## TODO
+- Character movement
+- Sound
+
+## Credits
+- Art by [Kenney](https://www.kenney.nl/)
