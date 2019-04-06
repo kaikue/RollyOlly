b7d35d2abfa4cc7a2ecd6ccaa5510af421d7cd0e
Kai Kuehner <kvk@pobox.com>
2019-04-06 14:00:18 -0400

    moving platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	public float speed;
+	public Transform[] nodes;
+
+	private Rigidbody2D rb;
+	private int targetNode = 0;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		NextNode();
+	}
+
+	private void NextNode()
+	{
+		transform.position = nodes[targetNode].position;
+		int oldNode = targetNode;
+		targetNode = (targetNode + 1) % nodes.Length;
+		Vector2 diff = nodes[targetNode].position - nodes[oldNode].position;
+		rb.velocity = diff.normalized * speed;
+
+		float time = diff.magnitude / speed;
+		Invoke("NextNode", time);
+	}
+}
