1fee5dbbb1ac2cd06282d5096646e6510dd11320
kaikue <kvk@pobox.com>
2019-04-07 20:21:32 -0400

    tile work and rotating platforms

---
+++ Assets/Scripts/RotatingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody2D rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		rb.angularVelocity = speed;
+	}
+}
